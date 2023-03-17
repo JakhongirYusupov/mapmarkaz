c82c98e8b76bc03352b65fa3cf6b428ebad97a7b
Jakhongir <jakhongiryusupov58@gmail.com>
2023-03-17 13:31:20 +0500

    home page done

--- src/Pages/Home/components/Content/Content.jsx
+++ src/Pages/Home/components/Content/Content.jsx
@@ -1,17 +1,135 @@
 import "./Content.scss";
 import { useTranslation } from 'react-i18next';
+import { CiSearch } from 'react-icons/ci';
+import { FiDownload } from 'react-icons/fi';
+import { AiOutlineEye } from "react-icons/ai";
+import { IoIosArrowDown } from 'react-icons/io';
+import { useState } from "react";
 
 export default function Content() {
   const { t } = useTranslation();
+  const [activeElonBar, setactiveElonBar] = useState(null);
 
   return (
     <section className="home-content">
       <div className="container">
         <div className="home-content-finance">
           <h2 className="home-content-title">{t("home.content.finance.title")}</h2>
-          <div className="home-content-finance-action"></div>
+          <div className="home-content-finance-action">
+            <div className="home-content-finance-action-date">
+              <div className="home-content-finance-action-date-year">
+                <span className="home-content-finance-action-date-title" >{t("home.content.finance.year")}</span>
+                <select name="" id="">
+                  <option value="2022">2022</option>
+                  <option value="2021">2021</option>
+                  <option value="2020">2020</option>
+                </select>
+              </div>
+              <div className="home-content-finance-action-date-kvartal">
+                <span className="home-content-finance-action-date-title">{t("home.content.finance.kvartal")}</span>
+                <div className="home-content-finance-action-date-kvartal-pagination">
+                  <div className="home-content-finance-action-date-kvartal-pagination-item"><span>1</span></div>
+                  <div className="home-content-finance-action-date-kvartal-pagination-item"><span>2</span></div>
+                  <div className="home-content-finance-action-date-kvartal-pagination-item"><span>3</span></div>
+                  <div className="home-content-finance-action-date-kvartal-pagination-item"><span>4</span></div>
+                  <div className="home-content-finance-action-date-kvartal-pagination-search">
+                    <span>{t("home.content.finance.search")}</span>
+                    <CiSearch className="home-content-finance-action-date-kvartal-pagination-search-icon" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="home-content-finance-action-border"></div>
+            <div className="home-content-finance-action-sendFile">
+              <div className="home-content-finance-action-sendFile-title">
+                <p>{t("home.content.finance.filepdf")}</p>
+              </div>
+              <div className="home-content-inputs">
+                <div className="home-content-sendInput">
+                  <label htmlFor="sendFile">
+                    <span>{t("home.content.download")}</span>
+                    <FiDownload className="home-content-sendInput-icon" />
+                  </label>
+                  <input type="file" id="sendFile" />
+                </div>
+                <button className="home-content-seeButton">
+                  <span>{t("home.content.see")}</span>
+                  <AiOutlineEye className="home-content-seeButton-icon" />
+                </button>
+              </div>
+            </div>
+          </div>
+
         </div>
-        <div className="home-content-elon"></div>
+        <div className="home-content-elon">
+          <h2 className="home-content-title">{t("home.content.elon.title")}</h2>
+          <div className="home-content-finance-action  home-content-elon-item"
+            style={activeElonBar === 1 ? { height: "182px" } : null}>
+            <h3 className="home-content-elon-title"
+              onClick={() => setactiveElonBar((el) => el === 1 ? null : 1)} >
+              <span>{t("home.content.elon.item1")}</span>
+              <IoIosArrowDown className="home-content-elon-title-arrowIcon"
+                style={activeElonBar === 1 ? { transform: "rotate(180deg)" } : null} />
+            </h3>
+            <div className="home-content-inputs">
+              <div className="home-content-sendInput">
+                <label htmlFor="sendFile">
+                  <span>{t("home.content.download")}</span>
+                  <FiDownload className="home-content-sendInput-icon" />
+                </label>
+                <input type="file" id="sendFile" />
+              </div>
+              <button className="home-content-seeButton">
+                <span>{t("home.content.see")}</span>
+                <AiOutlineEye className="home-content-seeButton-icon" />
+              </button>
+            </div>
+          </div>
+          <div className="home-content-finance-action  home-content-elon-item"
+            style={activeElonBar === 2 ? { height: "182px" } : null}>
+            <h3 className="home-content-elon-title"
+              onClick={() => setactiveElonBar((el) => el === 2 ? null : 2)} >
+              <span>{t("home.content.elon.item2")}</span>
+              <IoIosArrowDown className="home-content-elon-title-arrowIcon"
+                style={activeElonBar === 2 ? { transform: "rotate(180deg)" } : null} />
+            </h3>
+            <div className="home-content-inputs">
+              <div className="home-content-sendInput">
+                <label htmlFor="sendFile">
+                  <span>{t("home.content.download")}</span>
+                  <FiDownload className="home-content-sendInput-icon" />
+                </label>
+                <input type="file" id="sendFile" />
+              </div>
+              <button className="home-content-seeButton">
+                <span>{t("home.content.see")}</span>
+                <AiOutlineEye className="home-content-seeButton-icon" />
+              </button>
+            </div>
+          </div>
+          <div className="home-content-finance-action  home-content-elon-item"
+            style={activeElonBar === 3 ? { height: "182px" } : null}>
+            <h3 className="home-content-elon-title"
+              onClick={() => setactiveElonBar((el) => el === 3 ? null : 3)} >
+              <span>{t("home.content.elon.item3")}</span>
+              <IoIosArrowDown className="home-content-elon-title-arrowIcon"
+                style={activeElonBar === 3 ? { transform: "rotate(180deg)" } : null} />
+            </h3>
+            <div className="home-content-inputs">
+              <div className="home-content-sendInput">
+                <label htmlFor="sendFile">
+                  <span>{t("home.content.download")}</span>
+                  <FiDownload className="home-content-sendInput-icon" />
+                </label>
+                <input type="file" id="sendFile" />
+              </div>
+              <button className="home-content-seeButton">
+                <span>{t("home.content.see")}</span>
+                <AiOutlineEye className="home-content-seeButton-icon" />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,88 @@
+import { useTranslation } from "react-i18next";
+import logo from "../../assets/images/footer-logo.svg";
+import napa from "../../assets/images/footer-napa.svg";
+import { IoIosArrowUp } from 'react-icons/io';
 import "./Footer.scss";
 
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="footer-top">
+        <div className="container">
+          <div className="footer-top-wrapper">
+            <div className="footer-top-logo">
+              <img className="footer-top-logo-img" src={logo} alt="logo" />
+              <p className="footer-top-logo-desc">{t("footer.desc")}</p>
+            </div>
+            <div className="footer-top-navs">
+              <nav className="footer-top-nav">
+                <h4 className="footer-top-nav-title">{t("footer.contact.title")}</h4>
+                <ul className="footer-top-nav-list">
+                  <li>
+                    <a className="footer-top-nav-list-item-link" href="tel:+998712331737">
+                      <span>{t("footer.contact.item1")} +998 71 233-17-37</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a className="footer-top-nav-list-item-link" href="tel:+998712334344">
+                      <span>{t("footer.contact.item2")} +998 71 233-43-44</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a className="footer-top-nav-list-item-link" href="tel:+998993534922">
+                      <span>{t("footer.contact.item3")} +998 99 353-49-22</span>
+                    </a>
+                  </li>
+                  <a className="footer-top-nav-list-item-link" href="mailto:mapmarkaz@umail.uz"><span>mapmarkaz@umail.uz</span></a>
+                  <li>
+                    <p className="footer-top-nav-list-item-link">
+                      <span>{t("footer.contact.item4")}</span>
+                    </p>
+                  </li>
+                </ul>
+              </nav>
+              <nav className="footer-top-nav">
+                <h4 className="footer-top-nav-title">{t("footer.location.title")}</h4>
+                <ul className="footer-top-nav-list footer-top-nav-list-location">
+                  <li className="footer-top-nav-list-item-link">
+                    <span>{t("footer.location.item1")}</span>
+                    <span>{t("footer.location.item1_1")}</span>
+                  </li>
+                  <li className="footer-top-nav-list-item-link">
+                    <span>{t("footer.location.item2")}</span>
+                  </li>
+                  <li className="footer-top-nav-list-item-link">
+                    <span>{t("footer.location.item3")}</span>
+                  </li>
+                  <li className="footer-top-nav-list-item-link">
+                    <span>{t("footer.location.item4")}</span>
+                  </li>
+                  <li className="footer-top-nav-list-item-link">
+                    <span>{t("footer.location.item5")}</span>
+                  </li>
+                  <li className="footer-top-nav-list-item-link">
+                    <a href="#">{t("footer.location.link")}</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <div className="container">
+          <div className="footer-bottom-wrapper">
+            <div className="footer-bottom-desc">{t("footer.bottom_desc")}</div>
+            <div className="footer-bottom-scrollTop"
+              onClick={() => window.scrollTo(0, 0)}>
+              <IoIosArrowUp className="footer-bottom-scrollTop-icon" />
+            </div>
+            <img src={napa} alt="" className="footer-bottom-napa" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
